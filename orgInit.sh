d27bfae0b4f7d7cc8af2e7dc8c4dc8e30a4be155
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-02-24 10:10:42 -0500

    rename .orginit.sh to orginit.sh (remove . in front)

---
+++ orgInit.sh
@@ -0,0 +1,15 @@
+sfdx force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOrg -s
+
+sfdx force:source:push
+
+sfdx force:user:permset:assign -n Survey_Force_Admin
+
+sfdx force:org:open
+
+#Version Increase Comment
+
+
+#Convert sfdx source to mdapi as we still have src folder
+#sfdx force:source:convert -d src
+#Deploy to Dev Org: Use -c to check only first
+#sfdx force:mdapi:deploy -d src -u SurveyForceDevOrg -w 100
