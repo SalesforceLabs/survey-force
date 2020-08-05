c3da9a50888887ad3023dc83fdfcea3eb2fb0475
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-08-05 17:30:33 -0400

    Update deploy command in orgINit for packaging org

--- orgInit.sh
+++ orgInit.sh
@@ -9,7 +9,6 @@ sfdx force:org:open
 #Version Increase Comment
 
 
-#Convert sfdx source to mdapi as we still have src folder
-#sfdx force:source:convert -d src
 #Deploy to Survey force Dev/Packaging Org: Use -c to check only first
-#sfdx force:mdapi:deploy -d src -u SurveyForceDevOrg -w 100
+#
+#sfdx force:source:deploy -u SurveyForceDevOrg -w 100 -p force-app/main/default
