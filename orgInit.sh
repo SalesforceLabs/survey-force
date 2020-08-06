d952e9762e136e6a309997a998923a45be5c2da1
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-08-05 21:16:57 -0400

    Change inline edit to true on details tab so we can change fields there like enable/disable survey for guest. Also change default permission set for admin user in orgInit.sh to Survey_Force_SuperAdmin

--- orgInit.sh
+++ orgInit.sh
@@ -2,7 +2,7 @@ sfdx force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOr
 
 sfdx force:source:push
 
-sfdx force:user:permset:assign -n Survey_Force_Admin
+sfdx force:user:permset:assign -n Survey_Force_SuperAdmin
 
 sfdx force:org:open
 
