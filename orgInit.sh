363eada22e62989df0b0e53c0e156ada6772c90b
Jaswinder Rattanpal <jrattanpal@gmail.com>
2020-08-21 14:46:51 -0400

    Update pacakge version to 2.53

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,6 @@
 sfdx force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOrgPkgTest -s
 
-sfdx force:package:install -p 04t0H000000xUh0 -w 10
+sfdx force:package:install -p 04t0H000000xUh5 -w 10
 
 sfdx force:user:permset:assign -n Survey_Force_Admin
 
