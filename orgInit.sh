50df6bdccec3b581e9924a5f5ca2ddab4748009c
David Schach <dschach@x2od.com>
2023-08-14 16:50:27 -0700

    plugins: latest versions & prettify

--- orgInit.sh
+++ orgInit.sh
@@ -1,18 +1,17 @@
-#sfdx force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOrg -s
+#!/bin/sh
 
-sfdx org create scratch -f config/project-scratch-def.json -a SurveyForceScratchOrg -d 
+#sf force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOrg -s
+sf org create scratch --definition-file config/project-scratch-def.json --alias SurveyForceScratchOrg --set-default
 
-#sfdx force:source:push
-sfdx project deploy start
+#sf force:source:push
+sf project deploy start
 
-sfdx force:user:permset:assign -n Survey_Force_SuperAdmin
+sf force user permset assign --perm-set-name Survey_Force_SuperAdmin
 
-#sfdx force:org:open
-sfdx org open
+sf org open --target-org SurveyForceScratchOrg
 
 #Version Increase Comment
 
-
 #Deploy to Survey force Dev/Packaging Org: Use -c to check only first
 #
-#sfdx force:source:deploy -u SurveyForceDevOrg -w 100 -p force-app/main/default
+#sf project deploy start --target-org SurveyForceDevOrg --wait 100 --source-dir force-app/main/default
