4626cce5568e5ab1bcb8d5bfab0fdd9c4be0a186
David Schach <dschach@x2od.com>
2023-08-14 18:25:35 -0700

    test updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
+echo "Create new scratch org"
 #sf force:org:create -f config/project-scratch-def.json -a SurveyForceScratchOrg -s
 sf org create scratch --definition-file config/project-scratch-def.json --alias SurveyForceScratchOrg --set-default
 
+echo "Pushing metadata"
 #sf force:source:push
 sf project deploy start
 
+echo "Assigning Permissions"
 sf force user permset assign --perm-set-name Survey_Force_SuperAdmin
 
+echo "Opening Org"
 sf org open --target-org SurveyForceScratchOrg
 
 #Version Increase Comment
